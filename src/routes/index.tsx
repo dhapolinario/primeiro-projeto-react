6238e59bbb24fdf4aa2fd51b7e8d34c0aa9339c3
Dayvis Apolinário <dayvis.apolinario@gmail.com>
2020-05-17 16:33:33 -0300

    Configs: tsconfig, eslint, prettier, editorconfig. Routes. (wip) Pages.

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboad from '../pages/Dashboad';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboad} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
